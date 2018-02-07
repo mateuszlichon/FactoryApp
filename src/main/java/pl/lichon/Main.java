fbc3a225b02f7884f3d8d2d8cd806ae07a9a2008
mateuszlichon <mateusz.lichon@gmail.com>
2018-02-07 12:32:13 +0100

    adds dburl

--- src/main/java/pl/lichon/Main.java
+++ src/main/java/pl/lichon/Main.java
@@ -1,5 +1,6 @@
 package pl.lichon;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @SpringBootApplication
 public class Main {
+	
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
