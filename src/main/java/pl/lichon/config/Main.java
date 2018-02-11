d9082581a6e7eef31bc73bca3bf86c78a3913c66
mateuszlichon <mateusz.lichon@gmail.com>
2018-02-11 14:17:21 +0100

    modyfies main file

--- src/main/java/pl/lichon/config/Main.java
+++ src/main/java/pl/lichon/config/Main.java
@@ -1,25 +1,17 @@
 package pl.lichon.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
+@EnableScheduling
 public class Main {
-	
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Main.class, args);
-	}
-
-	@RequestMapping("/")
-	String index() {
-		return "index";
 	}
 
 }
