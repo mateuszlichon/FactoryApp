fc7ee2eb7e8afd3eb96933bed72645bc58b6c514
mateuszlichon <mateusz.lichon@gmail.com>
2018-02-13 07:04:13 +0100

    introduces BildingBlock entity

---
+++ src/main/java/pl/lichon/entity/BuildingBlock.java
@@ -0,0 +1,38 @@
+package pl.lichon.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BuildingBlock {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+
+	public BuildingBlock() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
