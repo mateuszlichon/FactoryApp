e64e7aec7d09174e7c8570ba8559bdc67338c12d
mateuszlichon <mateusz.lichon@gmail.com>
2018-02-15 06:43:54 +0100

    adds supervisor entity

---
+++ src/main/java/pl/lichon/entity/Supervisor.java
@@ -0,0 +1,49 @@
+package pl.lichon.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Supervisor {
+
+	private long id;
+	
+	private String firstName;
+	
+	private String lastName;
+
+	public Supervisor() {
+		super();
+	}
+
+	public Supervisor(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
